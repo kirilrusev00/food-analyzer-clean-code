c8bf83e125936d6ba23172d907a0d10fffaa3aa9
kiril <kiril.rusev2000@gmail.com>
2020-06-14 21:49:00 +0300

    Split function getRequiredInformation

--- src/bg/sofia/uni/fmi/mjt/food/server/retriever/barcode/BarcodeRetriever.java
+++ src/bg/sofia/uni/fmi/mjt/food/server/retriever/barcode/BarcodeRetriever.java
@@ -4,39 +4,46 @@ import bg.sofia.uni.fmi.mjt.food.server.retriever.FoodInfoRetriever;
 
 public class BarcodeRetriever {
 
+    private static final String START_PARAMETER_IMG = "--img=";
+    private static final String START_PARAMETER_CODE = "--code=";
+
     private BarcodeRetriever() {
 
     }
 
     public static String getRequiredInformation(String[] commandParts) {
-        boolean isCodeIncluded = false;
-        StringBuilder codeBuilder = new StringBuilder();
-        StringBuilder imgBuilder = new StringBuilder();
+        String code;
+        if (isCodeIncludedInCommand(commandParts)) {
+            code = getParameter(commandParts, START_PARAMETER_CODE, START_PARAMETER_IMG);
+        }
+        else {
+            String img = getParameter(commandParts, START_PARAMETER_IMG, START_PARAMETER_CODE);
+            code = QRCodeReader.getQrCode(img);
+        }
+        return FoodInfoRetriever.getGtinUpcCache().get(code);
+    }
+
+    private static String getParameter(String[] commandParts, String parameterStart, String nextParameterStart) {
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 1; i < commandParts.length; i++) {
-            if (commandParts[i].startsWith("--img=")) {
-                imgBuilder.append(commandParts[i]);
-                if (i + 1 < commandParts.length && !commandParts[i + 1].startsWith("--code=")) {
+            if (commandParts[i].startsWith(parameterStart)) {
+                stringBuilder.append(commandParts[i]);
+                if (i + 1 < commandParts.length && !commandParts[i + 1].startsWith(nextParameterStart)) {
                     i++;
-                    imgBuilder.append(" ").append(commandParts[i]);
-                }
-            }
-            if (commandParts[i].startsWith("--code=")) {
-                isCodeIncluded = true;
-                codeBuilder.append(commandParts[i]);
-                if (i + 1 < commandParts.length && !commandParts[i + 1].startsWith("--img=")) {
-                    i++;
-                    codeBuilder.append(" ").append(commandParts[i]);
+                    stringBuilder.append(" ").append(commandParts[i]);
                 }
                 break;
             }
         }
-        String code;
-        if (!isCodeIncluded) {
-            String img = imgBuilder.toString().replace("--img=", "");
-            code = QRCodeReader.getQrCode(img);
-        } else {
-            code = codeBuilder.toString().replace("--code=", "");
+        return stringBuilder.toString().replace(parameterStart, "");
+    }
+
+    private static boolean isCodeIncludedInCommand(String[] commandParts) {
+        for (int i = 1; i < commandParts.length; i++) {
+            if (commandParts[i].startsWith(START_PARAMETER_CODE)) {
+                return true;
+            }
         }
-        return FoodInfoRetriever.getGtinUpcCache().get(code);
+        return false;
     }
 }

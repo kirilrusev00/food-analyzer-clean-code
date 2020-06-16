8c8e30865a6628c76d63a5c2c8c25e86937aee56
kiril <kiril.rusev2000@gmail.com>
2020-06-16 10:37:09 +0300

    Remove cache interface

--- src/bg/sofia/uni/fmi/mjt/food/server/cache/InMemoryCache.java
+++ src/bg/sofia/uni/fmi/mjt/food/server/cache/InMemoryCache.java
@@ -4,22 +4,23 @@ import java.lang.ref.SoftReference;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class InMemoryCache<K, V> implements Cache<K, V> {
+public class InMemoryCache<K, V> {
 
     private static final int CLEAN_UP_PERIOD_IN_SEC = 5;
+    private static final long PERIOD_TO_STAY_IN_CACHE_IN_MILLIS = 86400000L; // 1 day
 
     private final ConcurrentHashMap<K, SoftReference<CacheObject<V>>> cache = new ConcurrentHashMap<>();
 
-    public InMemoryCache() {
+    InMemoryCache() {
         Thread cleanerThread = new Thread(() -> {
             while (!Thread.currentThread().isInterrupted()) {
                 try {
                     Thread.sleep(CLEAN_UP_PERIOD_IN_SEC * 1000);
                     cache.entrySet()
                             .removeIf(entry -> Optional.ofNullable(entry.getValue())
-                            .map(SoftReference::get)
-                            .map(CacheObject::isExpired)
-                            .orElse(false));
+                                    .map(SoftReference::get)
+                                    .map(CacheObject::isExpired)
+                                    .orElse(false));
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
@@ -29,25 +30,23 @@ public class InMemoryCache<K, V> implements Cache<K, V> {
         cleanerThread.start();
     }
 
-    @Override
-    public void add(K key, V value, long periodInMillis) {
+    public void add(K key, V value) {
         if (key == null) {
             return;
         }
         if (value == null) {
             cache.remove(key);
         } else {
-            long expiryTime = System.currentTimeMillis() + periodInMillis;
+            long expiryTime = System.currentTimeMillis() + PERIOD_TO_STAY_IN_CACHE_IN_MILLIS;
             cache.put(key, new SoftReference<>(new CacheObject<>(value, expiryTime)));
         }
     }
 
-    @Override
-    public void remove(K key) {
-        cache.remove(key);
-    }
-
-    @Override
+    /*
+        private void remove(K key) {
+            cache.remove(key);
+        }
+    */
     public V get(K key) {
         return Optional.ofNullable(cache.get(key))
                 .map(SoftReference::get)
@@ -55,13 +54,11 @@ public class InMemoryCache<K, V> implements Cache<K, V> {
                 .map(CacheObject::getValue)
                 .orElse(null);
     }
-
-    @Override
+    /*
     public void clear() {
         cache.clear();
     }
 
-    @Override
     public long size() {
         return cache.entrySet()
                 .stream()
@@ -70,7 +67,7 @@ public class InMemoryCache<K, V> implements Cache<K, V> {
                         .map(cacheObject -> !cacheObject.isExpired())
                         .orElse(false))
                 .count();
-    }
+    }*/
 
     private static class CacheObject<V> {
 
